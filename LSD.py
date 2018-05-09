e66f25d6f33422be94a5895f6ede9a73aca7182b
Yi Ding <fredericyiding@gmail.com>
2018-05-09 13:59:12 -0500

    Fix bugs in LSD.py: 1. count has to be initiated inside for loop (i.e., everytime we do a new digit) 2. self.a = self.aux must be outside of the for loop at the end of the code

--- LSD.py
+++ LSD.py
@@ -7,25 +7,31 @@ class LSD:
 				for string in line.split():
 					self.a.append(string)
 		self.W = len(self.a[0])
-		self.R = 256
+		self.R = 256  # can also be global variable
 		self.N = len(self.a)
-		self.aux = [0] * self.N
+		self.aux = [None] * self.N
 
 
 	def sort(self):
-		count = [0] * (self.R + 1)
-		for d in reversed(range(self.W - 1)):
 
+		for d in reversed(range(self.W)):
+			count = [0] * (self.R + 1)  
+			# Very important: tne count has to be initiated inside the for loop
 			for i in range(self.N):
-				count[ord(self.a[i][d]) - ord('a') + 1] += 1
+				count[ord(self.a[i][d]) + 1] += 1
 
 			for r in range(self.R):
 				count[r + 1] += count[r]
+			print count
 
 			for i in range(self.N):
-				self.aux[count[ord(self.a[i][d]) - ord('a')]] = self.a[i]
-				count[ord(self.a[i][d]) - ord('a')] += 1
+				print 'a[i][d]: ' + str(self.a[i][d])
+				print 'ord:' + str(ord(self.a[i][d]))  
+				print 'count: ' + str(count[ord(self.a[i][d])])
+				print 'aux: ' + str(self.aux[count[ord(self.a[i][d])]])
+				self.aux[count[ord(self.a[i][d])]] = self.a[i]
+				count[ord(self.a[i][d])] += 1
 
-			self.a = self.aux
+		self.a = self.aux
 		
 		return self.a
