3a8950961288e018e18a6971100717eaf97b993e
Yi Ding <fredericyiding@gmail.com>
2018-05-11 15:11:19 -0500

    Modify numsIslands.py by getting rid of enclosed functions and providing ways to select BFS and DFS in the API

--- numsIslands.py
+++ numsIslands.py
@@ -7,38 +7,54 @@ class Solution:
 	Attributes:
 	"""
 
-	def numsIslands(self, grid):
+	def numsIslands(self, grid, method = 'bfs'):
 		"""This function calculates the number of Islands.
 		Both BFS and DFS implementations were presented.
-
-		BFS can be implemented by both Queue.Queue or collections.deque
+		Note that BFS can be implemented by both Queue.Queue or collections.deque
 
 		Args:
-			grid: a list of lists with boolean input.
+			grid (list of lists): boolean input, 1 as island and 0 as sea.
+			method (string): bfs or dfs
 
-			example to generate a m x n grid:
+			example for generating a m x n grid:
 			>>> from random import randint
 			>>> m, n = 8, 7
 			>>> grid = [[randint(0, 2) for y in range(n)] for x in range(m)]
 
 		Returns:
 			An integer that is the number of islands, defined as segregated 1's.
+		
+
 		"""
-
-		m = len(grid)
-		if m == 0:
+		self.grid = grid
+		self.m = len(grid)
+		if self.m == 0:
 			return 0
-		n = len(grid[0])
+		self.n = len(grid[0])
+		self.visited = [[False for y in range(self.n)] for x in range(self.m)]
 
-		visited = [[False for y in range(n)] for x in range(m)]
+		count = 0
+		for row in range(self.m):
+			for col in range(self.n):
+				if self.check(row, col):
+					if method == 'bfs':
+						self.bfs(row, col)
+					elif method == 'dfs':
+						self.dfs(row, col)
+					else:
+						break
+					count += 1
 
-		def check(x, y):
-			return x >= 0 and x < m and y >= 0 and y < n and grid[x][y] and not visited[x][y]
+		return count
 
-		def bfs(x, y):
-			q = Queue(maxsize = m * n)
+	def check(self, x, y):
+
+			return x >= 0 and x < self.m and y >= 0 and y < self.n and self.grid[x][y] and not self.visited[x][y]
+
+	def bfs(self, x, y):
+			q = Queue(maxsize = self.m * self.n)
 			q.put((x, y))
-			visited[x][y] = True
+			self.visited[x][y] = True
 			nbrow = [0, 1, 0, -1]
 			nbcol = [1, 0, -1, 0]
 
@@ -46,27 +62,20 @@ class Solution:
 				x_q, y_q = q.get()
 				for i in range(4):
 					newx, newy = x_q + nbrow[i], y_q + nbcol[i]
-					if check(newx, newy):
+					if self.check(newx, newy):
 						q.put((newx, newy))
-						visited[newx][newy] = True
+						self.visited[newx][newy] = True
 
-		def dfs(x, y):
-			visited[x][y] = True
+	def dfs(self, x, y):
+			self.visited[x][y] = True
 			nbrow = [0, 1, 0, -1]
 			nbcol = [1, 0, -1, 0]
 
 			for i in range(4):
 				newx, newy = x + nbrow[i], y + nbcol[i]
-				if check(newx, newy):
-					visited[newx][newy] = True
-					dfs(newx, newy)
+				if self.check(newx, newy):
+					self.visited[newx][newy] = True
+					self.dfs(newx, newy)
 
-		count = 0
-		for row in range(m):
-			for col in range(n):
-				if check(row, col):
-					dfs(row, col) # or dfs(row, col)
-					count += 1
 
-		return count
 
